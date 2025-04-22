30906ff510c7e2af54758ecc94a55e60deb10a67
Asadus16 <asadslayer999@gmail.com>
2025-04-22 17:24:31 +0500

    first commit

---
+++ src/json/landingPageData.js
@@ -0,0 +1,185 @@
+/* eslint-disable import/extensions */
+
+// Services Img Imports
+import Web from '../assets/images/Services/Web.png';
+import Mobile from '../assets/images/Services/Mobile.png';
+import UIUX from '../assets/images/Services/Design.png';
+
+// Portfolio Img Imports
+import Recruiting from '../assets/images/Portfolio/Recruiting.png';
+import Stream from '../assets/images/Portfolio/Stream.png';
+import Freelance from '../assets/images/Portfolio/Freelance.png';
+import Aura from '../assets/images/Portfolio/Aura.png';
+import Surtido from '../assets/images/Portfolio/Surtido.png';
+import ManagementApp from '../assets/images/Portfolio/ManagementApp.png';
+
+// Advantages
+import Communicative from '../assets/images/Advantages/Communicative.png';
+import Collaborative from '../assets/images/Advantages/Collaborative.png';
+import Management from '../assets/images/Advantages/Management.png';
+import Favorite from '../assets/images/Advantages/Favorite.png';
+
+// Testimonials
+import Sasha from '../assets/images/Testimonials/Sasha.jpg';
+import Reiner from '../assets/images/Testimonials/Reiner.jpg';
+import Kruger from '../assets/images/Testimonials/Kruger.jpg';
+
+// TeamMembers
+import CEO from '../assets/images/TeamMembers/CEO.jpg';
+import HRD from '../assets/images/TeamMembers/HRD.jpg';
+import Finance from '../assets/images/TeamMembers/Finance.jpg';
+
+export const Services = [
+  {
+    title: 'Web Development',
+    imageUrl: Web,
+    animation: 'left',
+  },
+  {
+    title: 'Mobile Development',
+    imageUrl: Mobile,
+    animation: 'up',
+  },
+  {
+    title: 'UI/UX Design',
+    imageUrl: UIUX,
+    animation: 'right',
+  },
+];
+
+export const Portfolios = [
+  {
+    id: 'asd1293uasdads1',
+    title: 'Recruiting App',
+    imageUrl: Recruiting,
+    type: 'Mobile Apps',
+    responsibility: [
+      'Mobile Development',
+      'UI/UX Design',
+    ],
+    credit: 'https://dribbble.com/shots/15164950-Recruiting-app',
+  },
+  {
+    id: 'asd1293uhjkhkjh2',
+    title: 'Stream+',
+    imageUrl: Stream,
+    type: 'Mobile Apps',
+    responsibility: [
+      'Mobile Development',
+      'UI/UX Design',
+    ],
+    credit: 'https://dribbble.com/shots/15276430-Stream',
+  },
+  {
+    id: 'asd1293uvbvcbbd3',
+    title: 'Freelance',
+    imageUrl: Freelance,
+    type: 'Mobile Apps',
+    responsibility: [
+      'Mobile Development',
+      'UI/UX Design',
+    ],
+    credit: 'https://dribbble.com/shots/15223131-Freelance-Mobile-App-Concept',
+  },
+  {
+    id: 'asd1293ufgdfgs4',
+    title: 'Aura',
+    imageUrl: Aura,
+    type: 'Website',
+    responsibility: [
+      'Web Development',
+      'UI/UX Design',
+    ],
+    credit: 'https://dribbble.com/shots/15176338-Aura-Website-Main-Page',
+  },
+  {
+    id: 'asd1293ulskmnb5',
+    title: 'Surtido Rico',
+    imageUrl: Surtido,
+    type: 'Website',
+    responsibility: [
+      'Web Development',
+      'UI/UX Design',
+    ],
+    credit: 'https://dribbble.com/shots/15173118-Surtido-Rico',
+  },
+  {
+    id: 'asd1293ulkmnbj6',
+    title: 'Courses Management',
+    imageUrl: ManagementApp,
+    type: 'Website',
+    responsibility: [
+      'Web Development',
+      'UI/UX Design',
+    ],
+    credit: 'https://dribbble.com/shots/15197890-Courses-Management-and-Promoting-Web-App',
+  },
+];
+
+export const Advantages = [
+  [{
+    title: 'Communicative',
+    description: 'We communicate our project ideas and progress to make it clear.',
+    imageUrl: Communicative,
+  },
+  {
+    title: 'Management',
+    description: 'We manage our project properly to make our project done well.',
+    imageUrl: Management,
+  }],
+  [{
+    title: 'Collaborative​',
+    description: 'Our team are very collaborative to make our project done well.',
+    imageUrl: Collaborative,
+  },
+  {
+    title: 'Favorite',
+    description: "We've did so many project and all of our client love it.",
+    imageUrl: Favorite,
+  }],
+];
+export const Testimonials = [
+  {
+    id: 1,
+    name: 'Sasha Rose',
+    company: 'Owner, Surveyor Corps',
+    testimoni: 'Thanks for DevZode, you guys are the best! Keep up the great work!',
+    imageUrl: Sasha,
+  },
+  {
+    id: 2,
+    name: 'Kruger Khan',
+    company: 'Director, Shultan Oil',
+    testimoni: 'I just wanted to let you know that it’s been great working with DevZode.',
+    imageUrl: Kruger,
+  },
+  {
+    id: 3,
+    name: 'Reiner John',
+    company: 'CEO, Marley CO',
+    testimoni: 'DevZode is so great. Thank you so much for a job well done.',
+    imageUrl: Reiner,
+  },
+];
+
+export const TeamMembers = [
+  {
+    name: 'Asad ullah',
+    position: 'Full Stack Web developer',
+    experience: '2 Years',
+    imageUrl: CEO,
+  },
+  {
+    name: 'Sufian Kamran',
+    position: 'Full Stack Web developer',
+    experience: '3 Years',
+    imageUrl: HRD,
+  },
+  {
+    name: 'Ali Raza',
+    position: 'Full Stack Web developer',
+    experience: '1.5 Years',
+    imageUrl: Finance,
+  },
+
+];
